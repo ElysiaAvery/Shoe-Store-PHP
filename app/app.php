eab4026dd4c27d4aa01f4f504708ef3586427716
Staff <epicodus_staff@staff.local>
2017-02-01 14:18:31 -0800

    Add styling and further routing

--- app/app.php
+++ app/app.php
@@ -7,7 +7,7 @@
 
   $app = new Silex\Application();
 
-  $server = 'mysql:host=localhost;dbname=shoes';
+  $server = 'mysql:host=127.0.0.1:8889;dbname=shoes';
   $username = 'root';
   $password = 'root';
   $DB = new PDO($server, $username, $password);
@@ -21,6 +21,22 @@
 
   $app->get("/", function() use ($app) {
     return $app['twig']->render('index.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+  });
+
+  $app->get("/stores", function() use ($app) {
+    return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+  });
+
+  $app->post("/stores", function() use ($app) {
+    return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+  });
+
+  $app->get("/brands", function() use ($app) {
+    return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+  });
+
+  $app->post("/brands", function() use ($app) {
+    return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
   });
 
 
